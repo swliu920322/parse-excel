5fe09faa275bc2f869d16a86dd576accdf52b305
MLIHS0 <max.liu@scania.com>
2023-09-05 16:04:48 +0800

    make a tag after change the data flow

---
+++ src/stores/data.store.ts
@@ -0,0 +1,35 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import { fetchData } from '@/util/request'
+
+function dealChildrenRelation(data) {
+  const { rootApp, integrated } = data
+  console.log('data', data)
+  rootApp.forEach(i => {
+    const { 'Independent App': parName, 'Target Due Date': rootDate } = i
+    let isDelay = false
+    i.children = integrated.filter(ii => ii['Root-App'] === parName).map(i => {
+      const innerDelay = new Date(i['Target Due Date']) > new Date(rootDate)
+      isDelay = isDelay || innerDelay
+      return {
+        ...i,
+        'Independent App': i['Sub-App'],
+        isDelay: innerDelay
+      }
+    })
+    i.isDelay = isDelay
+  })
+}
+
+export const useDataStore = defineStore('data', () => {
+  const dataRef = ref({})
+
+  async function getData() {
+    const { data } = await fetchData()
+    dealChildrenRelation(data)
+  }
+
+  return {
+    data: dataRef
+  }
+})
